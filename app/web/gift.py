05cf478377c25d5df607a8025c5c29197f591448
zhou <prince5460@126.com>
2019-04-16 16:58:05 +0800

    撤销礼物与心愿

--- app/web/gift.py
+++ app/web/gift.py
@@ -1,7 +1,9 @@
-from flask import current_app, flash, redirect, url_for,render_template
+from flask import current_app, flash, redirect, url_for, render_template
 from flask_login import login_required, current_user
 
+from app.libs.enums import PendingStatus
 from app.models.base import db
+from app.models.drift import Drift
 from app.models.gift import Gift
 from app.view_models.gift import MyGifts
 from app.view_models.trade import MyTrades
@@ -42,6 +44,21 @@ def save_to_gifts(isbn):
     return redirect(url_for('web.book_detail', isbn=isbn))
 
 
+# 撤销赠送书籍
 @web.route('/gifts/<gid>/redraw')
+@login_required
 def redraw_from_gifts(gid):
-    pass
+    gift = Gift.query.filter_by(id=gid, launched=False).first_or_404()
+    # 判断这个礼物是否处于交易状态，
+    # 处于交易状态是不能直接撤销的
+    drift = Drift.query.filter_by(
+        gift_id=gid,
+        pending=PendingStatus.Waiting
+    ).first()
+    if drift:
+        flash('这个礼物正处于交易状态，请前往鱼漂完成该交易')
+    else:
+        with db.auto_commit():
+            current_user.beans -= current_app.config['BEANS_UPLOAD_ONE_BOOK']
+            gift.delete()
+    return redirect(url_for('web.my_gifts'))

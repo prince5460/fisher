3e91aad7b1d02ae96e982fae6d9c7176d1e1b099
zhou <prince5460@126.com>
2019-06-17 09:38:16 +0800

    fix bug

--- app/models/base.py
+++ app/models/base.py
@@ -28,7 +28,7 @@ class Query(BaseQuery):
     def filter_by(self, **kwargs):
         if 'status' not in kwargs.keys():
             kwargs['status'] = 1
-            return super(Query, self).filter_by(**kwargs)
+        return super(Query, self).filter_by(**kwargs)
 
 
 db = SQLAlchemy(query_class=Query)
